11b7d58a1df5710868987be23e922a171f4d61c1
Christian Gracia <42984214+ChristianGracia@users.noreply.github.com>
2022-01-20 19:06:18 -0600

    added some more kotlin code

--- Basics/Variables.kt
+++ Basics/Variables.kt
@@ -9,7 +9,7 @@ fun main() {
 
     // compiler can infer type based on initialized value but if mutating the var, the new value must be the same type as initalized value
     var typeTest = 6
-    print("${typeTest::class.simpleName}")
+    println("${typeTest::class.simpleName}")
 
     // String - double quotes - ""
     var textString: String = "some text"
@@ -19,7 +19,27 @@ fun main() {
     var textChar: Char = 'c'
     println(textChar)
 
+    // string concatenation
     val dog = "Chocolate Lab"
     val food = "Cheese"
     println("This " + dog + " loves " + food)
+
+    // string templates
+    val plant = "orchid"
+    val potSize = 6
+    val dayNum = 7 
+
+    val templateString = "An $plant in a $potSize inch pot must be watered every $dayNum days."
+    println(templateString)
+
+    // get input from user
+    println("Please type your name and hit Enter.")
+    val name = readLine()
+    println("Your name is $name!")
+
+    // compile to jar through CLI
+    // kotlinc Variables.kt -include-runtime -d Variables.jar
+
+    // run with
+    // java -jar Variables.jar
 }
